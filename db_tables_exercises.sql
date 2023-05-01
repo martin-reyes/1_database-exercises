314009fed21b4393e724964aae4caeaf4ee75bad
Martin Reyes <martin.anthony.reyes@gmail.com>
2023-05-01 14:33:10 -0500

    finished mysql_intro file and started on data_tables_exercises

---
+++ db_tables_exercises.sql
@@ -0,0 +1,27 @@
+-- 1. open mysql workbench and log in to db server
+-- 2. save work in this sql file
+SHOW DATABASES; -- 3. List dbs
+USE albums_db; -- 4. Use albums_db
+SELECT database(); -- 5. show current db
+SHOW TABLES; -- 6. list tables-- 
+
+USE employees_db; -- 7. switch to employees_db
+-- SELECT database(); -- 8. show current db
+-- SHOW TABLES; -- 9. list tables
+/*
+10. What different data types are present on this table?
+
+11. Which table(s) do you think contain a numeric type column?
+
+12. Which table(s) do you think contain a numeric type column?
+
+13. Which table(s) do you think contain a date type column? 
+
+14. What is the relationship between the employees and the departments tables?
+
+*/
+-- SHOW CREATE TABLE dept_manager; -- Show the SQL that created the dept_manager table
+/*
+Write the SQL it takes to show this as your exercise solution.
+
+*/
