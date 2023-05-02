5f34a11a45d88bdaa660ac35029b6c56e5f33a48
Martin Reyes <martin.anthony.reyes@gmail.com>
2023-05-02 14:16:35 -0500

    exercise 6 almost done, i have a question

--- 6_limit_exercises.sql
+++ 6_limit_exercises.sql
@@ -1,13 +1,34 @@
-
--- Create a new SQL script named limit_exercises.sql.
-
--- MySQL provides a way to return only unique results from our queries with the keyword DISTINCT. For example, to find all the unique titles within the company, we could run the following query:
--- SELECT DISTINCT title FROM titles;
-
+USE employees;
+SELECT * FROM employees.employees;
+/* 1. MySQL provides a way to return only unique results from our queries with the keyword DISTINCT. 
+		e.g. to find all the unique titles within the company, we could run the following query:
+*/
+SELECT DISTINCT title FROM titles;
 -- List the first 10 distinct last name sorted in descending order.
+SELECT DISTINCT last_name FROM employees.employees
+ORDER BY last_name DESC
+LIMIT 10;
 
--- Find all previous or current employees hired in the 90s and born on Christmas. Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. Write a comment in your code that lists the five names of the employees returned.
-
--- Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the query to find the tenth page of results.
-
--- LIMIT and OFFSET can be used to create multiple pages of data. What is the relationship between OFFSET (number of results to skip), LIMIT (number of results per page), and the page number?
+/* 3. Find all previous or current employees hired in the 90s and born on Christmas.
+		Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records.
+        Write a comment in your code that lists the five names of the employees returned.
+        query below
+*/
+SELECT * FROM employees.employees
+WHERE hire_date LIKE '199%' 
+	AND hire_date LIKE '%12-25'
+ORDER BY hire_date
+LIMIT 10; -- Yinghua Dredge, Jinpo Langford, Tiina Speek, Takahira Spataro, Jaber Piveteau
+/* 4. Try to think of your results as batches, sets, or pages. The first five results are your first page. 
+		The five after that would be your second page, etc. Update the query to find the tenth page of results.
+		LIMIT and OFFSET can be used to create multiple pages of data.
+        What is the relationship between OFFSET (number of results to skip),
+        LIMIT (number of results per page), and the page number?
+        OFFSET = page_number * LIMIT
+        query below
+*/
+SELECT * FROM employees.employees
+WHERE hire_date LIKE '199%' 
+	AND hire_date LIKE '%12-25'
+ORDER BY hire_date
+LIMIT 5 OFFSET 1; 
